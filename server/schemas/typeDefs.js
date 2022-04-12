d6ea8e89166021e256f08391949c745d7293ef7a
cmseibel <cam.m.seib@gmail.com>
2022-04-11 17:27:59 -0700

    Write type definitions for back-end graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require("apollo-server-express");
+
+module.exports = gql`
+    type Book {
+        bookId: String
+        title: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+    }
+
+    type User {
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    input BookInput {
+        bookId: String
+        title: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String, password: String): Auth
+        addUser(username: String, email: String, password: String): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: ID): User
+    }
+`;
