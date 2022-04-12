3bc3cf8370856e32e9972b342a29db9cbcb898ab
cmseibel <cam.m.seib@gmail.com>
2022-04-11 17:56:33 -0700

    Modify routes and server to use GraphQL

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { resolvers, typeDefs } = require("./schemas");
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +24,13 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
