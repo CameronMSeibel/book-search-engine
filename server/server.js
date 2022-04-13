3b2497d3ca5dbbdf190a3e55ce1f96753e0f5ed4
cmseibel <cam.m.seib@gmail.com>
2022-04-13 13:12:57 -0700

    Don't print all our env variables cx

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-console.log(process.env);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
